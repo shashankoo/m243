8c6c433985dddd6b54bdbdfa1ff69b47816acab7
wclshashank <shashank@williamscoomerce.com>
2022-09-23 15:14:20 +0530

    theme installed

--- app/etc/config.php
+++ app/etc/config.php
@@ -319,7 +319,7 @@ return [
         'Magento_ThemeGraphQl' => 1,
         'Magento_CmsSampleData' => 1,
         'Magento_Translation' => 1,
-        'Magento_TwoFactorAuth' => 1,
+        'Magento_TwoFactorAuth' => 0,
         'Magento_GoogleOptimizer' => 1,
         'Magento_Ups' => 1,
         'Magento_SalesSampleData' => 1,
